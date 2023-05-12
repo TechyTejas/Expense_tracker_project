ab5baaa096a1f51bf7d5e8b4cf302d3b723b3a64
tejas <sadadetej.27@gmail.com>
2023-05-12 14:24:57 +0530

    first_step

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,9 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React */}
+        {/* </a>
+      </header> */}
+      <h2>lets get started..!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
