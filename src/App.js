d1d7b229e97ceaef2309b3553a79839ec21da6f9
tejas <sadadetej.27@gmail.com>
2023-05-14 22:32:11 +0530

    Passing Data using loop

--- src/App.js
+++ src/App.js
@@ -3,26 +3,49 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses=[
+    { id:'e1',title:'Car Insurance',amount:299.99,date:new Date(2020,7,14),LocationOfExpenditure :"Nashik"},
+    { id:'e2',title:'New TV',amount:125.99,date:new Date(2020,8,19),LocationOfExpenditure :"Pune"},
+    { id:'e3',title:'New AC',amount:165.99,date:new Date(2020,10,21),LocationOfExpenditure :"Pune"},
+    { id:'e4',title:'Pinnut Butter',amount:100.99,date:new Date(2020,11,19),LocationOfExpenditure :"Mumbai"},
+  ]
+  const expenseItems = expenses.map((expense) => {
+    return <ExpenseItem 
+      key={expense.id} // You should always provide a unique key prop when rendering an array of components
+      title={expense.title} 
+      amount={expense.amount} 
+      date={expense.date} 
+      location={expense.LocationOfExpenditure}
+    />;
+  });
+
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React */}
-        {/* </a>
-      </header> */}
       <h2>lets get started..!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenseItems}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+  //     {/* <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+      //     Learn React */}
+      //   {/* </a>
+      // </header> */}
+  //
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-const expenseDate = new Date(2023,5,12)
-const expenseTitle = 'Car Insurance'
-const expenseAmount = 299.97
-const LocationOfExpenditure = "Nashik"
+function ExpenseItem(props) {
+// const expenseDate = new Date(2023,5,12)
+// const expenseTitle = 'Car Insurance'
+// const expenseAmount = 299.97
+// const LocationOfExpenditure = "Nashik"
+
 
   return (
      <div className="expense-item">
-      <div className="expense-date"> {expenseDate.toISOString()}</div>
+      <div className="expense-date"> {props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle} </h2>
-        <h2>{LocationOfExpenditure}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title} </h2>
+        <h2>{props.location } </h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
      </div>
   );
